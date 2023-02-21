e251b4a743f690f114af08f31a8f70e58e4666b0
qa-tfenn-3-tppk@example.com <qa-tfenn-3-tppk@example.com>
2023-02-21 21:58:44 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15356.stoplight-dev.com/api/v1/projects/cHJqOjE3Mw/spectral.js?branch_slug=main&token=f3da21bd-6170-456c-8089-4e50123e9bdc";
+export default { extends: ruleset };
